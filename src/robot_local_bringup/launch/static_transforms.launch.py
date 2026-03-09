ce97206678ffa6010b5a0b2c48c3809e64652cae
Jarvis-op <idurgamp@gmail.com>
2026-03-09 14:02:12 +0530

    modified:   src/robot_local_bringup/launch/static_transforms.launch.py

--- src/robot_local_bringup/launch/static_transforms.launch.py
+++ src/robot_local_bringup/launch/static_transforms.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     lidar_tf= Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments= ['0', '0', '0.14', '0', '0', '0', 'base_link', 'laser']
+        arguments= ['0', '0', '0.14', '0', '0', '0', 'odom', 'laser']
     )
 
     return LaunchDescription([imu_tf, lidar_tf])
